1cf5d13dbe829381c669f0a73a31d2c2ceee52e3
Divyanshu951 <divyanshuk9515@gmail.com>
2026-04-20 15:25:33 +0530

    add padding to the bottom of the list

--- src/App.jsx
+++ src/App.jsx
@@ -36,9 +36,9 @@ function App() {
         />
       )}
 
-      <main className="mx-auto w-full max-w-5xl flex-1 px-4 pt-5 md:pt-12">
+      <main className="mx-auto w-full max-w-5xl flex-1 px-4 py-5 md:py-12">
         <button
-          className="mb-5 w-full cursor-pointer rounded-2xl border border-amber-700/50 bg-stone-900/80 px-2 py-2 text-xl font-medium tracking-wide text-amber-500 shadow-xl backdrop-blur-md transition-all duration-300 hover:scale-[1.01] hover:bg-stone-800 disabled:opacity-50 md:mb-10 md:px-4 md:py-4"
+          className="sticky top-2 z-5 mb-5 w-full cursor-pointer rounded-2xl border border-amber-700/50 bg-stone-900/80 px-2 py-2 text-xl font-medium tracking-wide text-amber-500 shadow-xl backdrop-blur-md transition-all duration-300 hover:scale-[1.01] hover:bg-stone-800 disabled:opacity-50 md:mb-10 md:px-4 md:py-4"
           onClick={() => setIsModelVisible((prev) => !prev)}
         >
           + Add New Task
